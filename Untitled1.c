6bf75604b677ab5e97aa7dff58c4064b4a6acb94
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2022-08-04 18:07:02 -0300

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <time.h>
+
+//Escopo de funções
 int algoritDemorado(int n);
 
 int main (){
-	clock_t t1;
-	int n;
-	scanf("%i", &n);
-	t1 = clock ();
-	algoritDemorado(n);
-	t1 = clock () - t1;
+	clock_t t1; //Criando uma variavel que armazena o tempo do relogio
+	int n; //Variavel do tipo int
+	scanf("%i", &n); //Leitura de variavel do tipo int
+	t1 = clock (); //Atribuindo tempo atual do relogio a variavel de tempo criada anteriormente
+	algoritDemorado(n); //Chamada de uma função
+	t1 = clock () - t1; //Atribuindo novo valor de tempo a variavel de tempo criada anteriormente, neste caso, subtraimos o tempo atual do tempo antigo que estava armazenado na variavel de tempo "t1"
 	
-	printf(" %f \n", (((float)t1)/10)/CLOCKS_PER_SEC);
+	printf(" %f \n", (((float)t1)/10)/CLOCKS_PER_SEC); //Mostra o tempo em segundos que levou parar executar o algoritmo
 	
 	return 0;
 }
-
+//Função usada para teste de execução e complexidade
 int algoritDemorado(int n){
 	int soma = 0;
 	
 	int i, j, k, l; 
-	
+	//Soma numero de cada variavel de 0 até n
 	for (i = 0; i < n; i++){
 		for (j = 0; j < n; j++){
 			for (k = 0; k < n; k++){
@@ -31,5 +33,5 @@ int algoritDemorado(int n){
 			}
 		}
 	}
-	return soma;
+	return soma; //Retorna a soma de todos os valores de 0 até n
 }
