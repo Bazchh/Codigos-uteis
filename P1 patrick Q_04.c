0be23fc05cc62776482dbee9fff3ba1f469d14ed
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-13 10:55:19 -0300

    Update P1 patrick Q_04.c

--- P1 patrick Q_04.c
+++ P1 patrick Q_04.c
@@ -7,9 +7,11 @@ Marcos Mikael Lima Vidal
 Matricula: 2021010988
 */
 
+/*Programa que armazena dados de uma estrutura e verifica quais os maiores valores com base no desejado que est√£o dentro da estrutura*/
+
 /*ex entrada e saida
 
-entrada-> ** DADOS DO ATLETA N∫ 1 **
+entrada-> ** DADOS DO ATLETA N¬∫ 1 **
 
 Insira o nome do 1 atleta: mikael
 
@@ -20,7 +22,7 @@ Insira a idade do 1 atleta: 20
 Insira a altura do 1 atleta: 1,74
 
 
-** DADOS DO ATLETA N∫ 2 **
+** DADOS DO ATLETA N¬∫ 2 **
 
 Insira o nome do 2 atleta: marcos
 
@@ -31,20 +33,20 @@ Insira a idade do 2 atleta: 18
 Insira a altura do 2 atleta: 1,70
 
 
-** DADOS DO ATLETA N∫ 3 **
+** DADOS DO ATLETA N¬∫ 3 **
 
 Insira o nome do 3 atleta: Patrick
 
-Insira o esporte do 3 atleta: nataÁ„o
+Insira o esporte do 3 atleta: nata√ß√£o
 
 Insira a idade do 3 atleta: 38
 
 Insira a altura do 3 atleta: 1,76
 
 
-** DADOS DO ATLETA N∫ 4 **
+** DADOS DO ATLETA N¬∫ 4 **
 
-Insira o nome do 4 atleta: Jo„o
+Insira o nome do 4 atleta: Jo√£o
 
 Insira o esporte do 4 atleta: Roquei
 
@@ -53,9 +55,9 @@ Insira a idade do 4 atleta: 17
 Insira a altura do 4 atleta: 1,62
 
 
-** DADOS DO ATLETA N∫ 5 **
+** DADOS DO ATLETA N¬∫ 5 **
 
-Insira o nome do 5 atleta: VÙ Antonio
+Insira o nome do 5 atleta: V√¥ Antonio
 
 Insira o esporte do 5 atleta: futsal
 
@@ -70,7 +72,7 @@ saida-> Atleta com a maior idade: V" Antonio
 
 */
 
-//Struct que contÈm os dados dos atletas
+//Struct que cont√©m os dados dos atletas
 typedef struct atleta{
 	
 	char nick[30];
@@ -84,16 +86,16 @@ int main (){
 	setlocale(LC_ALL,"");
 	
 	int i, j; //Variaveis contadoras
-	int maiorA; //Variavel que servir· para armazenar a idade do atleta mais velho
-	float altH;//Variavel que servir· armazenar a altura do atleta mais alto
+	int maiorA; //Variavel que servir√° para armazenar a idade do atleta mais velho
+	float altH;//Variavel que servir√° armazenar a altura do atleta mais alto
 	char nick1[30],nick2[30]; //Variaveis que serviram para armazenar o nome do atleta mais velho e o nome do atleta mais alto
 	
-	Atlet data [5];//Definindo variavel que armazenar· dados dos 5 atletas
+	Atlet data [5];//Definindo variavel que armazenar√° dados dos 5 atletas
 	
 	//Inserindo dados dos 5 atletas
 	for(i=0;i < 5;i++){
 		
-	printf("\n***** DADOS DO ATLETA N∫ %i *****\n",i+1);	
+	printf("\n***** DADOS DO ATLETA N¬∫ %i *****\n",i+1);	
 			
 	printf("\nInsira o nome do %i atleta: ", i+1);
 	fgets(data[i].nick,30,stdin);
@@ -112,7 +114,7 @@ int main (){
 
 	altH = data[0].alt;
 	
-	//LaÁo que verifica qual atleta mais velho e mais alto e armazena seu nome em uma variavel char que ser· impressa como resultado
+	//La√ßo que verifica qual atleta mais velho e mais alto e armazena seu nome em uma variavel char que ser√° impressa como resultado
 	for(i=0;i<5;i++){	
 		if(data[i].age >= maiorA){	
 			maiorA = data[i].age;
